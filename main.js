fec0137986b05670d297cb53e8aaf9ee91bdee8f
Rosa Ponce Espín <yosoyrosaponce@gmail.com>
2024-01-09 10:05:12 +0100

    update randomMove and create compareUserMove

--- main.js
+++ main.js
@@ -1,30 +1,71 @@
 'use strict'
 
 const btn = document.querySelector('.js-btn');
+const select = document.querySelector('.js-select');
+const resultMsg = document.querySelector('.js-result');
 
 
 
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * 9);
+    const number = Math.ceil(Math.random() * 9);
+    return number;
     };
 
-let randomNumer = getRandomNumber();
-
+   
 
 const getRandomMove = () => {
+    let randomNumer = getRandomNumber();
     if(randomNumer <= 3) {
-        return "piedra"
-    } if (randomNumer >= 7) {
+        console.log("piedra")
+        return "piedra" 
+        
+    } else if (randomNumer >= 7) {
+        console.log("papel")
         return "papel"
+        
     } else {
+        console.log("tijera")
         return "tijera"
+        
     }
+
+    
 
 };
 
 
-const handleClick = () = {
-    if ()
-}
 
-btn.addEventListener('click', handleClick);
+
+const compareUserMove = () => {
+    const randomMove = getRandomMove();
+    if (select.value === randomMove) {
+        resultMsg.innerHTML = "Empate"
+    } else if (
+        (select.value === "piedra" && (randomMove === "tijera" || randomMove === "papel")) ||
+        (select.value === "tijera" && randomMove === "papel") ||
+        (select.value === "papel" && randomMove === "piedra")
+    ) {
+        resultMsg.innerHTML = "¡Has ganado!"
+    } else {
+        resultMsg.innerHTML = "Has perdido"
+    };
+    
+    
+     
+};
+
+const handleClick = (event) => {
+    event.preventDefault()
+    getRandomNumber()
+    compareUserMove()
+    
+};
+
+btn.addEventListener('click', handleClick); 
+
+
+
+
+
+
+
