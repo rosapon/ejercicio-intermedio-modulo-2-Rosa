9b396a8e3a59aed1c122e93282ebaa67a52b24b0
Rosa Ponce Espín <yosoyrosaponce@gmail.com>
2024-01-09 10:40:24 +0100

    sum points

--- main.js
+++ main.js
@@ -3,15 +3,18 @@
 const btn = document.querySelector('.js-btn');
 const select = document.querySelector('.js-select');
 const resultMsg = document.querySelector('.js-result');
+let player = document.querySelector('.js-player');
+let computer = document.querySelector('.js-computer');
+let playerPoints = 0;
+let computerPoints = 0;
 
 
 
 function getRandomNumber(max) {
     const number = Math.ceil(Math.random() * 9);
     return number;
+    
     };
-
-   
 
 const getRandomMove = () => {
     let randomNumer = getRandomNumber();
@@ -26,42 +29,41 @@ const getRandomMove = () => {
     } else {
         console.log("tijera")
         return "tijera"
-        
     }
-
-    
-
 };
-
-
-
 
 const compareUserMove = () => {
     const randomMove = getRandomMove();
     if (select.value === randomMove) {
         resultMsg.innerHTML = "Empate"
+
     } else if (
         (select.value === "piedra" && (randomMove === "tijera" || randomMove === "papel")) ||
-        (select.value === "tijera" && randomMove === "papel") ||
-        (select.value === "papel" && randomMove === "piedra")
-    ) {
+        (select.value === "tijera" && (randomMove === "papel")) ||
+        (select.value === "papel" && (randomMove === "piedra")) 
+        ) {
         resultMsg.innerHTML = "¡Has ganado!"
+        playerPoints += 1;
+        player.innerHTML = "Jugadora: " + playerPoints;
+        
     } else {
         resultMsg.innerHTML = "Has perdido"
+        computerPoints += 1;
+        computer.innerHTML = "Computadora: " + computerPoints;
     };
-    
-    
      
 };
 
 const handleClick = (event) => {
     event.preventDefault()
-    getRandomNumber()
     compareUserMove()
     
 };
 
 btn.addEventListener('click', handleClick); 
+
+
+
 
 
 
